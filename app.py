18bbf4ae52ee338a2a4c98d7fb7b5e7ec16751f1
gabriel gramm <gabrielgramm@hotmail.com>
2025-05-07 13:17:11 +0200

    upload

--- app.py
+++ app.py
@@ -1,36 +1,31 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import requests
+import seaborn as sns
+import pandas as pd
+from io import StringIO
+import matplotlib.pyplot as plt
 
-url = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/datasets.txt'
-response = requests.get(url)
-
-# Check if the request was successful
-if response.status_code == 200:
-    print("Request successful!")
-    print(response.text)  # Print the content of the response (list of dataset names)
-else:
-    print(f"Request failed with status code {response.status_code}")
-
-
-st.title("DS basics")
-
-st.title("Seaborn Visualization")
 
 sns.set_theme(style="whitegrid")
 
-#load data
-planets = sns.load_dataset("planets")
+sns.set_theme(style="whitegrid")
 
-planets.columns
+# Load the example planets dataset
+url = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/planets.csv'
+response = requests.get(url)
+planets = pd.read_csv(StringIO(response.text))
 
-cmap = sns. cubehelix_palette(rot=.2, as_cmap=True)
-plot = sns.relplot(
-    data=planets, x='distance', y='orbital_period', hue='year', size='mass', palette=cmap, sizes=(10,200)
+sns.set_theme(style="whitegrid")
+
+cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
+g = sns.relplot(
+    data=planets,
+    x="distance", y="orbital_period",
+    hue="year", size="mass",
+    palette=cmap, sizes=(10, 200),
 )
-plot.set(xscale='log', yscale='log')
+g.set(xscale="log", yscale="log")
 g.ax.xaxis.grid(True, "minor", linewidth=.25)
 g.ax.yaxis.grid(True, "minor", linewidth=.25)
 g.despine(left=True, bottom=True)
+st.pyplot(g.fig)
